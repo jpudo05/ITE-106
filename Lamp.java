874e86b8dc02eea5fd566f87d2ce984f27d7742d
jpudo05 <jaroldperillo@gmail.com>
2024-10-03 16:11:04 +0800

    Create Lamp.java

---
+++ Lamp.java
@@ -0,0 +1,12 @@
+public class Lamp {
+	boolean isOn;
+	void turnOn() {
+		isOn = true;
+		System.out.println("Light on? " + isOn);
+	}
+	
+	void turnOff() {
+		isOn = false:
+		System.out.println("Light on? " + isOn);
+	}
+}
