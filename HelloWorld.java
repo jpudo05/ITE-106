1f62f18baaace7d90df13fc3cb561376c01c4c41
jpudo05 <jaroldperillo@gmail.com>
2024-11-21 16:14:30 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class HelloWorld {
+	public static void main (String [] args) {
+		JFrame frame = new JFrame ("My frame");
+		frame.setSize(220, 200);
+		frame. setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container contentPane = frame.getContentPane();
+		contentPane. setLayout(null);
+		
+		JButton button = new JButton ("Hello World");
+		button.addActionListener (new MyActionListener ());
+		button.setLocation(30, 30);
+		button.setSize(150, 100);
+		contentPane.add(button);
+		
+		frame.setVisible(true);
+		
+	}
+}
