65e51d82b53b7cd7dd905a11ccbb5ec27caec0b9
jpudo05 <jaroldperillo@gmail.com>
2025-01-20 15:29:24 +0800

    Create MyActionListener.java

---
+++ Menu/act1/MyActionListener.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MyActionLIstener implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		Toolkit.getDefaultToolkit().beep();
+
+	}
+}
